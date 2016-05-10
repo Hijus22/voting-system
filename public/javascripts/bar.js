e78ae2150116db5eefe8d43b3b37e8cb27d80819
Antonio Martinez <amgarin90@gmail.com>
2016-05-10 18:45:48 -0400

    First commit

---
+++ public/javascripts/bar.js
@@ -0,0 +1,6 @@
+var MenuBar1 = new Spry.Widget.MenuBar(
+    "MenuDesplegable",
+    {
+        imgDown:"SpryAssets/SpryMenuBarDownHover.gif",
+        imgRight:"SpryAssets/SpryMenuBarRightHover.gif"
+    });
---
+++ routes/sign.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Hijus on 10/5/16.
+ */
