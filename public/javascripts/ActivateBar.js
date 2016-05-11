d481f3f1d37dc55733a1bc5617576d0f0af37230
Antonio Martinez <amgarin90@gmail.com>
2016-05-11 14:37:12 -0400

    Menu bar working

--- public/javascripts/ActivateBar.js
+++ public/javascripts/ActivateBar.js
@@ -1,6 +1,18 @@
-var MenuBar1 = new Spry.Widget.MenuBar(
-    "MenuDesplegable",
-    {
-        imgDown:"SpryAssets/SpryMenuBarDownHover.gif",
-        imgRight:"SpryAssets/SpryMenuBarRightHover.gif"
-    });
+$(function(){
+    $("#MenuDesplegable").html(
+        "<li><a href='/'>Home</a></li>"+
+        "<li><a href='/'>Votings</a></li>"+
+        "<li><a class='MenuBarItemSubmenu' href='/'>Sign in</a>"+
+            "<ul>"+
+                "<li><a href='/'>Sign up</a></li>"+
+            "</ul>"+
+        "</li>"+
+        "<li><a href='/'>Contact</a></li>"
+    )
+    var MenuBar1 = new Spry.Widget.MenuBar(
+        "MenuDesplegable",
+        {
+            imgDown:"SpryAssets/SpryMenuBarDownHover.gif",
+            imgRight:"SpryAssets/SpryMenuBarRightHover.gif"
+        });
+});
